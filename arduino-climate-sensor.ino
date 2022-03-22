914e7e5f06b5a6b47659fb0e61d5f92391e93710
Bryan Kyle <bryan.kyle@gmail.com>
2022-03-22 07:20:02 -0700

    feature complete

--- arduino-climate-sensor.ino
+++ arduino-climate-sensor.ino
@@ -1,31 +1,45 @@
+
+#define DOUBLERESETDETECTOR_DEBUG false
+#define ESP_DRD_USE_SPIFFS true
+
 #include <WiFi.h>
 #include <ArduinoJson.h>
 #include <PubSubClient.h>
 #include <Adafruit_AHTX0.h>
 #include <Adafruit_SSD1306.h>
+#include <WiFiManager.h>
+#include <ESP_DoubleResetDetector.h>
+
+#define PIN(x) x
+#define SECONDS(x) (x*1000)
 
 // Constants initialized at compile-time.
-const int  I2C_SDA    = 2;
-const int  I2C_SDL    = 1;
-const char SSID[]     = "changeme";
-const char PSK[]      = "changeme";
-const char BROKER[]   = "broker.mqtthq.com";
-const int  PORT       = 1883;
-const char TOPIC_FORMAT = 
+const int           I2C_SDA         = PIN(2);
+const int           I2C_SDL         = PIN(1);
+const char          BROKER[]        = "broker.mqtthq.com";
+const int           PORT            = 1883;
+const char          TOPIC_PREFIX[]  = "devices/";
+const unsigned long UPDATE_INTERVAL = SECONDS(5);
 
 // Constants initialized at runtime.
 char DEVICE_ID[16];
-char TOPIC[32];
+char TOPIC[sizeof(TOPIC_PREFIX) + 64];
 
 // Runtime variables
 bool             initializing     = true;
 long             sequence         = 0;
 sensors_event_t  temperature;
 sensors_event_t  relativeHumidity;
-WiFiClient       client;
-PubSubClient     MQTT(client);
-Adafruit_AHTX0   AHT10;
-Adafruit_SSD1306 SSD1306(128, 64);
+
+// Value of millis() when sensor values were read and reported.
+unsigned long    lastUpdateMillis;
+
+DoubleResetDetector drd(2, 0x0);
+WiFiManager         wm;
+WiFiClient          client;
+PubSubClient        MQTT(client);
+Adafruit_AHTX0      AHT10;
+Adafruit_SSD1306    SSD1306(128, 64);
 
 
 int die(const String& msg) {
@@ -100,9 +114,16 @@ void updateDisplay() {
 void setup() {
   Serial.begin(115200);
   
+  // Check for a double-reset indicating that the device should be restored to its default state.
+  if (drd.detectDoubleReset()) {
+    Serial.println("Double Reset Detected");
+    wm.resetSettings();
+  }
+
   // Initialize runtime constants
   snprintf(DEVICE_ID, sizeof(DEVICE_ID), "%012llx", ESP.getEfuseMac());
-  snprintf(TOPIC, sizeof(TOPIC), "devices/%s", DEVICE_ID);
+  snprintf(TOPIC, sizeof(TOPIC), "%s%s", TOPIC_PREFIX, DEVICE_ID);
+
 
   // Setup peripherals
   Wire.setPins(I2C_SDA, I2C_SDL) || die(F("I2C pin configuration failed"));
@@ -112,15 +133,18 @@ void setup() {
   SSD1306.setTextColor(SSD1306_WHITE);
   updateDisplay();
 
-  // Setup WiFi connection
-  WiFi.begin(SSID, PSK) || die(F("WiFi initialization failed"));
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-  Serial.println();
-  Serial.println(WiFi.localIP());
 
+  // Configure the WiFi Manager
+  std::vector<const char*> menu = {"wifi", "param", "info", "update"};
+  wm.setMenu(menu);
+  wm.setConfigPortalBlocking(false);
+
+
+  // Attempt to connect to the WiFi network using the stored credentials.  If this is unsucessful
+  // then the configuration portal will automatically be started.
+  wm.autoConnect();
+
+  
   // Setup MQTT
   MQTT.setServer(BROKER, PORT);
 
@@ -130,9 +154,23 @@ void setup() {
 }
 
 void loop() {
-  // Connect to the MQTT broker if not already connected.  This will blick until a connection
+  // Clean up the Double Reset Detector state.
+  drd.loop();
+
+  
+  // Do WiFiManager related processing.
+  wm.process();
+
+
+  // The config portal (not to be confused with the web portal) will be started automatically
+  // from wm.autoConnect() in setup() if a connection cannot be made.  The web portal is
+  // started to allow re-configuration.
+  wm.startWebPortal();
+
+  
+  // Connect to the MQTT broker if not already connected.  This will block until a connection
   // has been established.
-  if (!MQTT.connected()) {
+  if (WiFi.isConnected() && !MQTT.connected()) {
     Serial.printf("Connecting to %s:%d...\n", BROKER, PORT);
     while (!MQTT.connected()) {
       char clientId[64];
@@ -148,31 +186,37 @@ void loop() {
     }
   }
 
+
   // Do MQTT protocol related processing.
   MQTT.loop();
 
-  // Read the values from the sensor
-  AHT10.getEvent(&relativeHumidity, &temperature);
 
-  updateDisplay();  
+  // Read sensors, update the display, and publish messages if a reasonable amount of
+  // time has passed.
+  if (millis() > lastUpdateMillis + UPDATE_INTERVAL) {
+    // Read the values from the sensor
+    AHT10.getEvent(&relativeHumidity, &temperature);
 
-  // Build the MQTT message
-  String message;
-  DynamicJsonDocument doc(1024);
-  doc[F("deviceId")] = DEVICE_ID;
-  doc[F("sequence")] = ++sequence;
-  doc[F("payload")][F("temperature")][F("value")] = temperature.temperature;
-  doc[F("payload")][F("temperature")][F("unit")] = F("C");
-  doc[F("payload")][F("humidity")][F("value")] = relativeHumidity.relative_humidity;
-  doc[F("payload")][F("humidity")][F("unit")] = F("%");
-  doc[F("payload")][F("rssi")][F("value")] = WiFi.RSSI();
-  doc[F("payload")][F("rssi")][F("unit")] = F("dBm");
-  serializeJson(doc, message);
+    updateDisplay(); 
+    
+    lastUpdateMillis = millis();
 
-  // Send the message to the MQTT broker and serial.
-  Serial.println(message);
-  MQTT.publish(TOPIC, message.c_str());
+    // Build the MQTT message
+    String message;
+    DynamicJsonDocument doc(1024);
+    doc[F("deviceId")] = DEVICE_ID;
+    doc[F("sequence")] = ++sequence;
+    doc[F("payload")][F("temperature")][F("value")] = temperature.temperature;
+    doc[F("payload")][F("temperature")][F("unit")] = F("C");
+    doc[F("payload")][F("humidity")][F("value")] = relativeHumidity.relative_humidity;
+    doc[F("payload")][F("humidity")][F("unit")] = F("%");
+    doc[F("payload")][F("rssi")][F("value")] = WiFi.RSSI();
+    doc[F("payload")][F("rssi")][F("unit")] = F("dBm");
+    serializeJson(doc, message);
+  
+    // Send the message to the MQTT broker and serial.
+    Serial.println(message);
+    MQTT.publish(TOPIC, message.c_str());
+  }
 
-
-  delay(5000);
 }
